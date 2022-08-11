a7e5e7a80ec0d3b023b488f31999ba7209111757
s13g <ayflix0@gmail.com>
2022-08-11 20:57:41 +0100

    final commit

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -6,5 +6,8 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:title>", views.entry, name="entry"),
     path("search/", views.search, name="search"),
-    path("create/", views.create, name="create")
+    path("create/", views.create, name="create"),
+    path("update/", views.update, name="update"),
+    path("save/", views.save, name="save"),
+    path("random/", views.random_entry, name="random")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,3 +1,5 @@
+import random
+
 from django.shortcuts import render
 from markdown2 import Markdown
 
@@ -43,4 +45,43 @@ def search(request):
 
 
 def create(request):
-    return render(request, "encyclopedia/create.html")
+    if request.method == "GET":
+        return render(request, "encyclopedia/create.html")
+    else:
+        title = request.POST['title']
+        content = request.POST['content']
+        title_exist = util.get_entry(title)
+        if title_exist is not None:
+            return render(request, "encyclopedia/error.html", {"message": "Entry page already exists"})
+        else:
+            util.save_entry(title, content)
+            converted_content = convert_md_to_html(title)
+            return render(request, "encyclopedia/entry.html",
+                          context={"title": title, "content": converted_content})
+
+
+def update(request):
+    if request.method == "POST":
+        title = request.POST['entry-title']
+        content = util.get_entry(title)
+        return render(request, "encyclopedia/update.html",
+                      context={"title": title,
+                               "content": content})
+
+
+def save(request):
+    if request.method == "POST":
+        title = request.POST['title']
+        content = request.POST['content']
+        util.save_entry(title,content)
+        converted_content = convert_md_to_html(title)
+        return render(request, "encyclopedia/entry.html",
+                      context={"title": title, "content": converted_content})
+
+
+def random_entry(request):
+    entries = util.list_entries()
+    random_entry = random.choice(entries)
+    content = convert_md_to_html(random_entry)
+    return render(request, "encyclopedia/entry.html",
+                  context={"title": random_entry, "content": content})
